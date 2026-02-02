eb4c4372e9745e20fb991dce57425eb4eb82ba83
hgguspet <gustav.pettersson.bjorklund@hitachigymnasiet.se>
2026-02-02 12:57:17 +0100

    maybe?

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,6 +6,17 @@
 
   ros2-humble = inputs.nix-ros-overlay.overlays.default;
 
+  # Fix for separateDebugInfo requiring __structuredAttrs in ROS2 packages
+  ros2-structuredAttrs = final: prev: {
+    rosPackages = prev.rosPackages or {} // {
+      humble = (prev.rosPackages.humble or {}) // (prev.lib.mapAttrs (name: pkg:
+        if prev.lib.isDerivation pkg && (pkg.separateDebugInfo or false)
+        then pkg.overrideAttrs (old: {__structuredAttrs = true;})
+        else pkg
+      ) (prev.rosPackages.humble or {}));
+    };
+  };
+
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
